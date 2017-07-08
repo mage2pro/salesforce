d4d896231195efe38818c33fb1f024c623886e2c
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-07-08 19:10:54 +0300

    no message

---
+++ T/Basic.php
@@ -0,0 +1,7 @@
+<?php
+namespace Dfe\Salesforce\T;
+// 2017-07-08
+final class Basic extends TestCase {
+	/** @test 2017-07-08 */
+	function t00() {}
+}
---
+++ T/TestCase.php
@@ -0,0 +1,5 @@
+<?php
+namespace Dfe\Salesforce\T;
+// 2017-07-08
+/** @see \Df\Zoho\T\Basic */
+abstract class TestCase extends \Df\Core\TestCase {}
