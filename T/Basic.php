9a11f8dda4c4e218df1f9b2d2c4e16c382720c0c
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-07-08 19:13:05 +0300

    no message

--- T/Basic.php
+++ T/Basic.php
@@ -4,4 +4,14 @@ namespace Dfe\Salesforce\T;
 final class Basic extends TestCase {
 	/** @test 2017-07-08 */
 	function t00() {}
+
+	/** 2017-07-08 */
+	function t01_services() {echo df_json_prettify(file_get_contents(
+		'https://mage2pro-dev-ed.my.salesforce.com/services/data.json'
+	));}
+
+	/** @test 2017-07-08 */
+	function t01_services2() {echo df_json_prettify(file_get_contents(
+		'https://mage2pro-dev-ed.lightning.force.com/services/data.json'
+	));}
 }
