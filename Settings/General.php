cab00b7f6e62a5589b996247c7d1f36d6e800d06
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-07-09 06:59:21 +0300

    no message

---
+++ Settings/General.php
@@ -0,0 +1,20 @@
+<?php
+namespace Dfe\Salesforce\Settings;
+// 2017-07-09
+/** @method static General s() */
+final class General extends \Df\Config\Settings {
+	/**
+	 * 2017-07-09 «Your Salesforce domain»
+	 * @return string
+	 */
+	function domain() {return $this->v();}
+
+	/**
+	 * 2017-07-09
+	 * @override
+	 * @see \Df\Config\Settings::prefix()
+	 * @used-by \Df\Config\Settings::v()
+	 * @return string
+	 */
+	final protected function prefix() {return 'df_salesforce/general';}
+}
