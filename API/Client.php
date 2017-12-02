d7d7fd1d8ce1790b51d1f201c1b3b3691e61060d
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-12-02 04:35:23 +0300

    0.0.8: https://github.com/mage2pro/core/issues/60

--- API/Client.php
+++ API/Client.php
@@ -61,10 +61,10 @@ final class Client extends \Df\API\Client {
 	/**
 	 * 2017-07-09
 	 * @override
-	 * @see \Df\API\Client::uriBase()
+	 * @see \Df\API\Client::urlBase()
 	 * @used-by \Df\API\Client::__construct()
 	 * @used-by \Df\API\Client::_p()
 	 * @return string
 	 */
-	protected function uriBase() {return sprintf('https://%s/', G::s()->domain());}
+	protected function urlBase() {return sprintf('https://%s/', G::s()->domain());}
 }
