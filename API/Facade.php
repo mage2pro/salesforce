dc82e239a15124cc1bb9ef38d651a5003e05c8f9
Dmitry Fedyuk <dfediuk@gmail.com>
2023-02-08 02:38:15 +0000

    https://github.com/mage2pro/core/issues/168

--- API/Facade.php
+++ API/Facade.php
@@ -24,7 +24,7 @@ final class Facade {
 	 * @return array(string => mixed)
 	 * @throws DFE
 	 */
-	private function p($path, array $p = [], $method = null) {return
+	private function p($path, array $p = [], $method = null):array {return
 		(new Client($path, $p, $method))->p()
 	;}
 
